a588bff4d00173e6a5a582f89cb1715907fc8134
Antoine R <coderademii@gmail.com>
2022-03-11 13:48:31 +0100

    React + bootstrap installation

---
+++ assets/js/Global/Menu.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <div className="menuItem text-danger">
+                    <p>Dashboard</p>
+                </div>
+                <div className="menuItem">
+                    <p>Dashboard</p>
+                </div>
+                <div className="menuItem">
+                    <p>Dashboard</p>
+                </div>
+                <div className="menuItem">
+                    <p>Dashboard</p>
+                </div>
+
+            </div>
+        );
+    }
+}
---
+++ assets/js/rep_log_react.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Home from "./Home/Home";
+import Menu from "./Global/Menu";
+
+const heart = true;
+
+render(
+    <Menu/>,
+    document.getElementById('test')
+);
+
+render(
+    <Menu/>,
+    document.getElementById('test')
+);
