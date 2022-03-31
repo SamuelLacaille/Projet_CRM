0f28fb4932ac9bf0205a8240955cfe4dcef494b9
Samuel Lacaille <samuel76560@outlook.fr>
2022-04-01 00:21:02 +0200

    full calendar bug fixe

--- src/Controller/CommercialController.php
+++ src/Controller/CommercialController.php
@@ -5,16 +5,38 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\PlanningRepository;
 
 class CommercialController extends AbstractController
 {
     /**
      * @Route("/commercial", name="commercial")
      */
-    public function index(): Response
+    public function index(PlanningRepository $calendrier)
     {
-        return $this->render('commercial/index.html.twig', [
-            'controller_name' => 'CommercialController',
-        ]);
+
+
+        $events = $calendrier->findAll();
+       // dd($events);
+
+        $rdv = [];
+
+        foreach($events as $event){
+            $rdv[] = [
+                'id' => $event->getId(),
+                'title'=> $event->getTitre(),
+                'start' => $event->getDebut()->format("Y-m-d H:i:s"),
+                'end' => $event->getFin()->format("Y-m-d H:i:s"),
+                'description' => $event->getDescription(),
+                'jourComplet' => $event->getJourComplet(),
+                'emailRdv' => $event->getEmailRdv(),
+                'emailCommercial' => $event->getEmailCommercial(),
+            ];
+        }
+
+        $data = json_encode($rdv);
+    
+ 
+        return $this->render('commercial/index.html.twig', compact('data'));
     }
 }
