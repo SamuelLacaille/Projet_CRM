9494b7ae389e615876c7d24630d6b1931501991a
SamuelLacaille <samuel76560@outlook.fr>
2022-04-01 10:46:13 +0200

    génération de pdf

---
+++ migrations/Version20220401074036.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220401074036 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE planning (id INT AUTO_INCREMENT NOT NULL, titre VARCHAR(255) NOT NULL, debut DATETIME NOT NULL, fin DATETIME NOT NULL, description LONGTEXT NOT NULL, jour_complet TINYINT(1) NOT NULL, email_rdv VARCHAR(255) NOT NULL, email_commercial VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE utilisateur (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_1D1C63B3E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE planning');
+        $this->addSql('DROP TABLE utilisateur');
+    }
+}
--- src/Controller/DevisController.php
+++ src/Controller/DevisController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 use Dompdf\Dompdf;
 use Dompdf\Options;
 
@@ -19,9 +20,8 @@ class DevisController extends AbstractController
   
 
 // instantiate and use the dompdf class
-$options = new Options();
-$options->set('defaultFont', 'Courier');
-$dompdf = new Dompdf($options);
+
+$dompdf = new Dompdf();
 
 $dompdf->loadHtml('hello world');
 
@@ -32,6 +32,7 @@ $dompdf->setPaper('A4', 'landscape');
 $dompdf->render();
 
 // Output the generated PDF to Browser
+ob_get_clean();
 $dompdf->stream();
 
         return $this->render('devis/index.html.twig', [
