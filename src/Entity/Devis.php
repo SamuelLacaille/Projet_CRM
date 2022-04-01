0b59ffd0571a0433a50d65a05879e616afe09026
SamuelLacaille <samuel76560@outlook.fr>
2022-04-01 11:19:51 +0200

    formulaire devis + entite

---
+++ src/Entity/Devis.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DevisRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=DevisRepository::class)
+ */
+class Devis
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nomSociete;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nomClient;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nomProduit;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $quantite;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $prixTtc;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomSociete(): ?string
+    {
+        return $this->nomSociete;
+    }
+
+    public function setNomSociete(string $nomSociete): self
+    {
+        $this->nomSociete = $nomSociete;
+
+        return $this;
+    }
+
+    public function getNomClient(): ?string
+    {
+        return $this->nomClient;
+    }
+
+    public function setNomClient(string $nomClient): self
+    {
+        $this->nomClient = $nomClient;
+
+        return $this;
+    }
+
+    public function getNomProduit(): ?string
+    {
+        return $this->nomProduit;
+    }
+
+    public function setNomProduit(string $nomProduit): self
+    {
+        $this->nomProduit = $nomProduit;
+
+        return $this;
+    }
+
+    public function getQuantite(): ?int
+    {
+        return $this->quantite;
+    }
+
+    public function setQuantite(int $quantite): self
+    {
+        $this->quantite = $quantite;
+
+        return $this;
+    }
+
+    public function getPrixTtc(): ?int
+    {
+        return $this->prixTtc;
+    }
+
+    public function setPrixTtc(int $prixTtc): self
+    {
+        $this->prixTtc = $prixTtc;
+
+        return $this;
+    }
+}
---
+++ src/Form/DevisType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Devis;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
+
+class DevisType extends AbstractType{
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nomClient')
+            ->add('nomSociete')
+            ->add('nomProduit')
+            ->add('quantite')
+            ->add('prixTtc')
+            ;
+    }
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Devis::class,
+        ]);
+    }
+}
